0762d5b5a7557b3fc4433b437095b9bacf18013c
lumachroma <b.blakk@gmail.com>
2018-12-15 16:29:40 +0800

    Call seed from main (call services from main)

---
+++ SimpleApp/Core/Models/SeedInMemoryData.cs
@@ -0,0 +1,36 @@
+﻿using SimpleApp.Infrastructure;
+using System;
+using System.Linq;
+
+namespace SimpleApp.Core.Models
+{
+    public static class SeedInMemoryData
+    {
+        public static void Initialize(AppDbContext context)
+        {
+            if (context.BrainstormSessions.ToList().Any())
+            {
+                return;
+            }
+            context.BrainstormSessions.Add(GetTestSession());
+            context.SaveChanges();
+        }
+
+        private static BrainstormSession GetTestSession()
+        {
+            var session = new BrainstormSession()
+            {
+                Name = "First Test Session",
+                DateCreated = DateTime.Now
+            };
+            var idea = new Idea()
+            {
+                DateCreated = DateTime.Now.AddMinutes(1),
+                Description = "Totally awesome idea",
+                Name = "Awesome idea"
+            };
+            session.AddIdea(idea);
+            return session;
+        }
+    }
+}
--- SimpleApp/Program.cs
+++ SimpleApp/Program.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SimpleApp.Core.Interfaces;
+using SimpleApp.Core.Models;
+using SimpleApp.Infrastructure;
 
 namespace SimpleApp
 {
@@ -14,7 +18,26 @@ namespace SimpleApp
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var serviceScope = host.Services.CreateScope())
+            {
+                var services = serviceScope.ServiceProvider;
+
+                try
+                {
+                    var repository = services.GetRequiredService<IBrainstormSessionRepository>();
+                    var context = services.GetRequiredService<AppDbContext>();
+                    SeedInMemoryData.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while creating service scope.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
