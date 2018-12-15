8d282459a9fdd64fd4de88e732c80c6ea2125009
lumachroma <b.blakk@gmail.com>
2018-12-15 13:37:31 +0800

    Add repository EFStormSessionRepository

---
+++ SimpleApp/Core/Interfaces/IBrainstormSessionRepository.cs
@@ -0,0 +1,14 @@
+﻿using SimpleApp.Core.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SimpleApp.Core.Interfaces
+{
+    public interface IBrainstormSessionRepository
+    {
+        Task<BrainstormSession> GetByIdAsync(int id);
+        Task<List<BrainstormSession>> ListAsync();
+        Task AddAsync(BrainstormSession session);
+        Task UpdateAsync(BrainstormSession session);
+    }
+}
