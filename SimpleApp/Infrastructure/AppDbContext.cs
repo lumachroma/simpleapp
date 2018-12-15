94d61325a58e62d634240e8deeb412766db72789
lumachroma <b.blakk@gmail.com>
2018-12-15 13:34:35 +0800

    Add dbcontext

---
+++ SimpleApp/Infrastructure/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleApp.Core.Models;
+
+namespace SimpleApp.Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) :
+           base(dbContextOptions)
+        {
+        }
+
+        public DbSet<BrainstormSession> BrainstormSessions { get; set; }
+    }
+}
