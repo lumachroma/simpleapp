db18feaa658e77657f8a18d7c8f3058a372f9be6
lumachroma <b.blakk@gmail.com>
2018-12-18 13:12:18 +0800

    Create TodoItem webapi using DapperRepository on MySql

---
+++ SimpleApp/Api/TodosController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleApp.Core.Interfaces;
+using SimpleApp.Core.Models;
+
+namespace SimpleApp.Api
+{
+    [Route("api/todos")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+        private readonly IRepository<TodoItem> _repository;
+
+        public TodosController(IRepository<TodoItem> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult All()
+        {
+            var result = _repository.List();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                var result = _repository.GetById(id);
+                if (null == result)
+                {
+                    return NotFound();
+                }
+                return Ok(result);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Add(TodoItem model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                _repository.Insert(model);
+            }
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Edit(TodoItem model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                _repository.Update(model);
+            }
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Remove(TodoItem model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                _repository.Delete(model);
+            }
+            return Ok();
+        }
+    }
+}
