a5416146be064e63f63940624a795bd91fcaa9f5
lumachroma <b.blakk@gmail.com>
2018-12-18 13:02:35 +0800

    Add generic repository interface

---
+++ SimpleApp/Core/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using SimpleApp.Core.Models;
+using System.Collections.Generic;
+
+namespace SimpleApp.Core.Interfaces
+{
+    public interface IRepository<T> where T : EntityBase
+    {
+        T GetById(int id);
+        IEnumerable<T> List();
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
