a5b3a6f4ca9e1ddf7d902bf1906e61155294a90a
lumachroma <b.blakk@gmail.com>
2018-12-18 13:11:35 +0800

    Create TodoItem model

---
+++ SimpleApp/Core/Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SimpleApp.Core.Models
+{
+    [Table("TodoItem")]
+    public class TodoItem : EntityBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime CompletedDate { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
