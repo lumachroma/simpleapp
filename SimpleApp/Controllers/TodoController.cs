632ab918e67424dfb2179814158397f3dfc6a56f
lumachroma <b.blakk@gmail.com>
2018-12-18 21:52:50 +0800

    Add TodoController and Manage Todos logics

---
+++ SimpleApp/Controllers/TodoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleApp.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
